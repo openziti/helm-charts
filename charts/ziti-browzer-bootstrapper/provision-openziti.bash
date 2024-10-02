ecb1bf6086b33732c143bd0890a2eb47b15bc38e
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-10-02 13:45:01 -0400

    button up the provisioning script

--- charts/ziti-browzer-bootstrapper/provision-openziti.bash
+++ charts/ziti-browzer-bootstrapper/provision-openziti.bash
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+#
+# inputs:
+#   ZITI_BROWZER_OIDC_URL   OIDC issuer url
+#   ZITI_BROWZER_CLIENT_ID  OIDC client id
+#   BROWZER_EMAILS          space or comma separated list of emails to create with role browzer.enabled.identities
+#
 
 set -o errexit
 set -o nounset
 set -o pipefail
 set -o xtrace
 
+# workaround: ext-jwt-signer cannot be updated, so dependency tree must be cleaned up first;
+#   https://github.com/openziti/ziti/issues/2459
 function cleanup() {
-    for EMAIL in "${EMAILS[@]}"; do
+    for EMAIL in "${EMAILS[@]}"
+    do
         ziti edge delete identity "${EMAIL}"
     done
     ziti edge delete auth-policy "${AUTH_POLICY_NAME}"
@@ -17,31 +26,39 @@ function cleanup() {
 : "${EXT_JWT_SIGNER_NAME:="browzer-auth0-ext-jwt-signer"}"
 : "${AUTH_POLICY_NAME:="browzer-auth0-auth-policy"}"
 
+BROWZER_EMAILS="${BROWZER_EMAILS//,/ }"
 typeset -a EMAILS=(${BROWZER_EMAILS})
 
-issuer=$(curl -s ${ZITI_BROWZER_OIDC_URL%/}/.well-known/openid-configuration | jq -r .issuer)
-jwks=$(curl -s ${ZITI_BROWZER_OIDC_URL%/}/.well-known/openid-configuration | jq -r .jwks_uri)
+oidc_config="$(curl -sSf ${ZITI_BROWZER_OIDC_URL%/}/.well-known/openid-configuration)"
+issuer="$(jq -r .issuer <<< "${oidc_config}")"
+jwks="$(jq -r .jwks_uri <<< "${oidc_config}")"
 
-echo "OIDC issuer   : $issuer"
-echo "OIDC jwks url : $jwks"
-
-if  ziti edge list ext-jwt-signers "name=\"$EXT_JWT_SIGNER_NAME\"" | grep -q $EXT_JWT_SIGNER_NAME; then
+if ziti edge list ext-jwt-signers "name=\"$EXT_JWT_SIGNER_NAME\"" | grep -q $EXT_JWT_SIGNER_NAME
+then
     cleanup
 fi
 ext_jwt_signer=$(ziti edge create ext-jwt-signer "${EXT_JWT_SIGNER_NAME}" "${issuer}" --jwks-endpoint "${jwks}" --audience "${ZITI_BROWZER_CLIENT_ID}" --claims-property ${ZITI_BROWZER_FIELD})
-echo "ext jwt signer id: $ext_jwt_signer"
 
-if  ziti edge list auth-policies "name=\"$AUTH_POLICY_NAME\"" | grep -q $AUTH_POLICY_NAME; then
+if ziti edge list auth-policies "name=\"$AUTH_POLICY_NAME\"" | grep -q $AUTH_POLICY_NAME
+then
     auth_policy=$(ziti edge update auth-policy "${AUTH_POLICY_NAME}" --primary-ext-jwt-allowed --primary-ext-jwt-allowed-signers ${ext_jwt_signer})
 else
     auth_policy=$(ziti edge create auth-policy "${AUTH_POLICY_NAME}" --primary-ext-jwt-allowed --primary-ext-jwt-allowed-signers ${ext_jwt_signer})
 fi
-echo "auth policy id: $auth_policy"
 
-for EMAIL in "${EMAILS[@]}"; do
-    if ziti edge list identities "name=\"${EMAIL}\"" | grep -q "${EMAIL}"; then
+for EMAIL in "${EMAILS[@]}"
+do
+    if ziti edge list identities "name=\"${EMAIL}\"" | grep -q "${EMAIL}"
+    then
         ziti edge update identity "${EMAIL}" --auth-policy ${auth_policy} --external-id "${EMAIL}" -a browzer.enabled.identities
     else
         ziti edge create identity "${EMAIL}" --auth-policy ${auth_policy} --external-id "${EMAIL}" -a browzer.enabled.identities
     fi
 done
+
+echo -e "\nissuer:" "$issuer\n"\
+        "jwks:" "$jwks\n"\
+        "ext-jwt-signer:" "$ext_jwt_signer\n"\
+        "auth-policy:" "$auth_policy\n"\
+        "email(s):" "${EMAILS[@]}\n" \
+| column -t
